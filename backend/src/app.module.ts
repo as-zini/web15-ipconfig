15c69004113b234c85a5eb43780f21b4230f4c29
snailw <lth010618@gmail.com>
2025-12-16 15:39:21 +0900

    feat: 위젯, 워크스페이스 모듈 기본 구조 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkspaceGateway } from './workspace/workspace.gateway';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { WidgetService } from './widget/widget.service';
+import { WidgetModule } from './widget/widget.module';
 
 @Module({
-  imports: [],
+  imports: [WorkspaceModule, WidgetModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WorkspaceGateway, WidgetService],
 })
 export class AppModule {}
