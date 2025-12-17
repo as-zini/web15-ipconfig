98f88fdf3cf5bc5493f3f20ab3c4e8dd8285abb0
davidpro08 <davidmoon0817@gmail.com>
2025-12-17 12:50:59 +0900

    fix: provider 등록 + 린트 오류 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WorkspaceGateway } from './workspace/workspace.gateway';
 import { WorkspaceModule } from './workspace/workspace.module';
-import { WidgetService } from './widget/widget.service';
 import { WidgetModule } from './widget/widget.module';
 
 @Module({
   imports: [WorkspaceModule, WidgetModule],
   controllers: [AppController],
-  providers: [AppService, WorkspaceGateway, WidgetService],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/widget/widget.gateway.ts
+++ backend/src/widget/widget.gateway.ts
@@ -1,9 +1,20 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class WidgetGateway {
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: unknown,
+  ): string {
+    void client;
+    void payload;
     return 'Hello world!';
   }
 }
