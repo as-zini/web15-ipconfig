6f10d85563a76ad951b0172f80d0afae0f3bbd7c
GyuriKim <artgr2062@konkuk.ac.kr>
2026-01-08 02:11:15 +0900

    fix: ReactPortalProps의 parent 속성을 선택적으로 변경

--- frontend/src/common/components/ReactPortal.tsx
+++ frontend/src/common/components/ReactPortal.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 import { createPortal } from 'react-dom';
 
 type ReactPortalProps = {
-  parent: HTMLElement;
+  parent?: HTMLElement;
 } & PropsWithChildren;
 
 export default function ReactPortal({
