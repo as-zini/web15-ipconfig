c50a1f827fd18919e6f4c626dff214380ac538ec
as-zini <aszini@naver.com>
2026-01-22 20:53:23 +0900

    refactor: 불필요한 파일 삭제

--- frontend/src/common/components/canvas/components/Canvas.tsx
+++ frontend/src/common/components/canvas/components/Canvas.tsx
@@ -1,6 +1,6 @@
 import CursorLayer from '@/common/components/cursor/CursorLayer';
 import WidgetLayer from '@/common/components/widgetFrame/WidgetLayer';
-import WidgetEditingPreviewLayer from '@/common/components/widgetFrame/WidgetEditingPreviewLayer';
+
 import { CanvasProvider } from '@/common/components/canvas/context/CanvasProvider';
 import ZoomControls from '@/common/components/canvas/components/ZoomControls';
 import { CanvasWrapper } from './CanvasWrapper';
@@ -10,8 +10,7 @@ function CanvasContent() {
     <CanvasWrapper>
       {/* 위젯 렌더링 */}
       <WidgetLayer />
-      {/* 다른 유저의 드래그/리사이즈 프리뷰 */}
-      <WidgetEditingPreviewLayer />
+
       {/* 커서 렌더링 */}
       <CursorLayer />
     </CanvasWrapper>
