4bcac59b15fd796cef05a620f63b3f237a7483a1
davidpro08 <davidmoon0817@gmail.com>
2026-01-29 19:08:46 +0900

    fix: 매핑 수정

--- frontend/src/features/export/hooks/mappingIcon.tsx
+++ frontend/src/features/export/hooks/mappingIcon.tsx
@@ -1,11 +1,12 @@
-import { LuFileJson, LuContainer } from 'react-icons/lu';
+import { SiPrettier } from 'react-icons/si';
+import { LuContainer } from 'react-icons/lu';
 
 export function mappingIcon(type: string) {
   switch (type) {
-    case 'prettier':
-      return <LuFileJson size={18} className="text-primary-600" />;
-    case 'docker':
-      return <LuContainer size={18} className="text-primary-600" />;
+    case 'CODE_FORMAT':
+      return <SiPrettier size={18} className="text-primary-600 h-5 w-5" />;
+    case 'DOCKER':
+      return <LuContainer size={18} className="text-primary-600 h-5 w-5" />;
     default:
       return null;
   }
