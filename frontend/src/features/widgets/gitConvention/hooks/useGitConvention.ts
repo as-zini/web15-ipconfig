6226f8282b26825534ab70eb208e5c8841828ea9
as-zini <aszini@naver.com>
2026-02-02 17:56:35 +0900

    fix: 전략 변경시 필드에 전달하는 값 변경

--- frontend/src/features/widgets/gitConvention/hooks/useGitConvention.ts
+++ frontend/src/features/widgets/gitConvention/hooks/useGitConvention.ts
@@ -93,7 +93,7 @@ export function useGitConvention() {
             widgetId,
             type,
             'developBranch',
-            rules.developBranch || '',
+            rules.developBranch || undefined,
           );
 
           if (rules.prefixes) {
